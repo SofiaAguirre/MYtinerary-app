22bc7abcf60c1eda8aa5b72554a018f3065383bc
SofiaAguirre <49541872+SofiaAguirre@users.noreply.github.com>
2020-01-24 13:07:41 -0300

    prueba2

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 require ('dotenv').config()
 require ('./src/database');
 const cors = require('cors')
-
+const path = require('path')
 app.use(cors());
 app.use (express.static('client'));
 app.use(bodyParser.json());
@@ -20,4 +20,11 @@ app.use('/users', require ('./routes/userRoutes'));
 app.use('/comment', require ('./routes/commentsRoutes'));
 app.use('/favourite', require ('./routes/favouriteRoutes'));
 const port = process.env.PORT || 5000;
+if (process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => { 
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 app.listen(port, () => console.log(`Server running on the best port ${port} !!`));
